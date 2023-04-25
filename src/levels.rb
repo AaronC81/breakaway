6a8ad0eb51dd042534506caa5bb060cdf720df14
Aaron Christiansen <aaronc20000@gmail.com>
2023-04-25 20:40:10 +0100

    Add a load of easy starting levels

--- src/levels.rb
+++ src/levels.rb
@@ -7,10 +7,62 @@ module GosuGameJam4
     end
 
     LEVELS = [
+        # _.__________|_
         Level.new(->do
             Player.register_new(x: 500, y: 500)
             Floor.register_new(x: 400, y: 500, width: 800, height: 50)
-            Flag.register_new(x: 1000, y: 500)
+            Flag.register_new(x: 1100, y: 500)
+        end),
+
+        #           __|_
+        #      ____
+        # _.__
+        Level.new(->do
+            Player.register_new(x: 500, y: 550)
+            Floor.register_new(x: 400, y: 550, width: 200, height: 50)
+            Floor.register_new(x: 700, y: 500, width: 200, height: 50)
+            Floor.register_new(x: 1000, y: 450, width: 200, height: 50)
+            Flag.register_new(x: 1100, y: 450)
+        end),
+
+        # _._          _|_
+        Level.new(->do
+            Player.register_new(x: 300, y: 500)
+            Floor.register_new(x: 200, y: 500, width: 300, height: 50)
+            Floor.register_new(x: 1100, y: 500, width: 300, height: 50)
+            Flag.register_new(x: 1300, y: 500)
+        end),
+
+        #              _|_
+        #
+        #
+        # _._          ___
+        Level.new(->do
+            Player.register_new(x: 300, y: 700)
+            Floor.register_new(x: 200, y: 700, width: 300, height: 50)
+            Floor.register_new(x: 1100, y: 700, width: 300, height: 50)
+            Floor.register_new(x: 1100, y: 300, width: 300, height: 50)
+            Flag.register_new(x: 1300, y: 300)
+        end),
+
+        # _____._____
+        # 
+        # 
+        #     _|_
+        Level.new(->do
+            Player.register_new(x: 400, y: 300)
+            Floor.register_new(x: 300, y: 300, width: 1000, height: 50)
+            Floor.register_new(x: 700, y: 800, width: 200, height: 50)
+            Flag.register_new(x: 800, y: 800)
+        end),
+
+        #       O
+        # _.____O____|_
+        Level.new(->do
+            Player.register_new(x: 500, y: 500)
+            Floor.register_new(x: 400, y: 500, width: 800, height: 50)
+            Wall.register_new(x: 750, y: 300, width: 100, height: 200)
+            Flag.register_new(x: 1100, y: 500)
         end),
 
         Level.new(->do
